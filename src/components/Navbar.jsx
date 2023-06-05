4e6d07edb98cdb9f97db560ae9d5924f2e00f63a
10Cynthia <98178163+10Cynthia@users.noreply.github.com>
2023-06-05 05:37:21 -0700

    finished navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
 
 const Navbar = () => (
   <Stack
@@ -10,7 +11,10 @@ const Navbar = () => (
   p={2}
   sx={{ position: 'sticky', background:'#000', top: 0, justifyContent: 'space-between'}} 
   >
-
+    <Link to='/' style={ { display: 'flex', alignItems: 'center' }}>
+      <img src={logo} alt='logo' height={45} />
+    </Link>
+    <SearchBar />
   </Stack>
 )
 
